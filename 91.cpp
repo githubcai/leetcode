df1de9a51b65a1a50b1f027ff2e386c8651ca051
caixukun <2468085704@qq.com>
2016-12-28 23:44:16 +0800

    1

--- 91.cpp
+++ 91.cpp
@@ -7,16 +7,20 @@ public:
             return 1;
         }
         int dp[s.size() + 1];
-        dp[0] = 0, dp[1] = 1;
+        dp[0] = 1, dp[1] = 1;
         if(s[0] == '0') return 0;
         for(int i = 2; i <= s.size(); ++i){
             int temp = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
             if(temp == 0) return 0;
             if(temp <= 26){
                 if(s[i - 1] != '0'){
-                    dp[i] = max(dp[i - 2] + 1, dp[i - 1] + 1);
+                    if(s[i - 2] != '0'){
+                        dp[i] = dp[i - 2] + dp[i - 1];
+                    }else{
+                        dp[i] = dp[i - 1];
+                    }
                 }else{
-                    dp[i] = dp[i - 2] + 1;
+                    dp[i] = dp[i - 2];
                 }
             }else{
                 if(s[i - 1] == '0') return 0;
